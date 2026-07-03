b062fdf1801f0ef43c3177cd7a991fce86c20431
anish <anishkr2842003@gmail.com>
2026-07-03 16:56:55 +0530

    update common route

--- src/common/exam/exam.common.routes.js
+++ src/common/exam/exam.common.routes.js
@@ -9,13 +9,18 @@ const User        = require('../../models/User.model')
 // GET /api/v1/common/exams
 // qualificationId is read fresh from DB so stale tokens don't return wrong results
 router.get('/', authMiddleware, catchAsync(async (req, res) => {
-  const user = await User.findById(req.user._id).select('qualification').lean()
+  const user = await User.findById(req.user._id).select('qualification exam').lean()
 
   if (!user?.qualification?._id) {
     throw new AppError('Please complete your profile and select a qualification first', 400, 'QUALIFICATION_NOT_SET')
   }
 
-  const exams = await examService.listByQualification(user.qualification._id)
+  let exams = await examService.listByQualification(user.qualification._id)
+
+  if (user?.exam?._id) {
+    exams = exams.filter((exam) => String(exam._id) === String(user.exam._id))
+  }
+
   sendSuccess(res, exams)
 }))
 
