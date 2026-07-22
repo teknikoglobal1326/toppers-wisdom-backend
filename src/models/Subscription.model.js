a9ec5c9333afc0b3ccc09a3624e6d702bcc3b794
teknikog6 <ritikyaduvanshi2004@gmail.com>
2026-07-22 18:02:18 +0530

    in subsciption add a banner field

--- src/models/Subscription.model.js
+++ src/models/Subscription.model.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose');
 
 const testItemSchema = new mongoose.Schema({
-  moduleType: { 
-    type: String, 
-    enum: ['TestSeries', 'PreviousYearPaper', 'LiveTestSeries'], 
-    required: true 
+  moduleType: {
+    type: String,
+    enum: ['TestSeries', 'PreviousYearPaper', 'LiveTestSeries'],
+    required: true
   },
-  moduleId: [{ 
-    type: mongoose.Schema.Types.ObjectId, 
+  moduleId: [{
+    type: mongoose.Schema.Types.ObjectId,
     required: true,
     refPath: 'tests.moduleType'
   }]
 }, { _id: false });
 
 const boosterItemSchema = new mongoose.Schema({
-  moduleType: { 
-    type: String, 
-    enum: ['Booster', 'Vocabulary', 'Editorial', 'vocabulary', 'editorial'], 
-    required: true 
+  moduleType: {
+    type: String,
+    enum: ['Booster', 'Vocabulary', 'Editorial', 'vocabulary', 'editorial'],
+    required: true
   },
-  moduleId: [{ 
-    type: mongoose.Schema.Types.ObjectId, 
+  moduleId: [{
+    type: mongoose.Schema.Types.ObjectId,
     required: true,
     refPath: 'boosters.moduleType'
   }]
@@ -31,10 +31,14 @@ const subscriptionSchema = new mongoose.Schema({
   description: { type: String },
   price: { type: Number, required: true },
   durationDays: { type: Number, required: true },
-  
-  tests: [testItemSchema], 
+  banner: {
+    type: String,
+    default: ""
+  },
+
+  tests: [testItemSchema],
   boosters: [boosterItemSchema],
-  
+
   isActive: { type: Boolean, default: true, index: true },
   isDeleted: { type: Boolean, default: false, index: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' }
