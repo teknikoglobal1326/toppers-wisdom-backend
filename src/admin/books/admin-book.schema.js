d8bf1c822e07e7fce331b3bcc5ca2f98c3b7ef9a
ritik yadav <ritik.yadav@baseel.com>
2026-07-26 14:19:30 +0530

    update in filters

--- src/admin/books/admin-book.schema.js
+++ src/admin/books/admin-book.schema.js
@@ -57,6 +57,7 @@ const listBookQuerySchema = Joi.object({
   sortOrder: Joi.string().valid('asc', 'desc').default('asc'),
   page: Joi.number().integer().min(1).default(1),
   limit: Joi.number().integer().min(1).max(100).default(10),
+  exam: Joi.string().optional(),
 })
 
 module.exports = { createBookSchema, createBookDualSchema, updateBookSchema, setBuyUrlSchema, listBookQuerySchema }
